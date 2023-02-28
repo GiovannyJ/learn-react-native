44431ab3f55de9361ff3d464cda62d1c22c05bd2
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2023-02-27 20:31:03 -0500

    added get, post, and patch method
    patch needs fixing

--- RNTest/App.tsx
+++ RNTest/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {StyleSheet, ActivityIndicator, FlatList, View, Button, SafeAreaView, Text, Alert} from 'react-native';
+import {StyleSheet, ActivityIndicator,TextInput, FlatList, View, Button, SafeAreaView, Text, Alert} from 'react-native';
 //struct modeled off our data
 type Weather = {
     id: number;
@@ -28,6 +28,12 @@ const styles = StyleSheet.create({
       borderBottomColor: '#737373',
       borderBottomWidth: StyleSheet.hairlineWidth,
     },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
   });
 
 const Separator = () => <View style={styles.separator} />
@@ -40,13 +46,19 @@ const App = () => {
     const[isLoading, setLoading] = useState(true);
     //bind them to be weather list
     const[data, setData] = useState<Weather[]>([]);
+    
+    const [temperature, setTemperature] = useState('');
+    const [humidity, setHumidity] = useState('');
+    const [precipitation, setPrecipitation] = useState('');
+    const [date, setDate] = useState('');
+    const [id] = useState('0');
 
     //calling api
     const getWeather = async () =>{
       const options = {
         method:'GET',
         header: {
-            'Content-Type': 'application/jsons',
+            'Content-Type': 'application/json',
         },
       }  
       try{
@@ -60,6 +72,58 @@ const App = () => {
         }
     };
 
+    const postWeather = async () =>{
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+         
+        },
+        body: JSON.stringify({
+            id:parseInt(id), 
+            temp:parseInt(temperature),
+            humid:parseInt(humidity),
+            percip:parseInt(precipitation),
+            date:date
+          }),
+      };
+          try{
+            const response = await fetch('http://localhost:8080/data',options);
+            const json = await response.json();
+            setData(json.data)
+          }catch(error){
+            console.error(error);
+          }finally{
+            setLoading(false);
+          }
+    };
+
+    const updateWeather = async () =>{
+      const options ={
+        method: 'PATCH',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id:parseInt(id),
+          temp:parseInt(temperature),
+          humid:parseInt(humidity),
+          percip:parseInt(precipitation),
+          date:date
+        }),
+        };
+        try{
+          const response = await fetch('http://localhost:8080/data?id='+'',options)
+          const json = await response.json();
+          setData(json.data)
+        }catch(error){
+          console.error(error);
+        }finally{
+          setLoading(false)
+        }
+
+    };
+
     //call api function
     // useEffect(() => {
     //     getWeather();
@@ -71,7 +135,7 @@ const App = () => {
           {isLoading ? (
             <Button title="Load API" onPress={() => getWeather()}/>
           ) : (
-            <><FlatList
+            <><><FlatList
                   data={data}
                   keyExtractor={(item) => item.date}
                   renderItem={({ item }) => (
@@ -80,6 +144,9 @@ const App = () => {
                       <Text style={styles.fixToText}>Temperature: {item.temp}</Text>
                       <Text>Humidity: {item.humid}</Text>
                       <Text>Precipitation: {item.percip}</Text>
+                      <Button
+                        title="Update"
+                        onPress={() => updateWeather()} />
                     </View>
                   )} />
                   <View style={styles.center}>
@@ -87,7 +154,46 @@ const App = () => {
                       title="Reset"
                       onPress={() => setLoading(true)} />
                   </View></>
+                  
+                  <View style={styles.center}>
+                  <Text>Temperature</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Temperature"
+                    keyboardType="numeric"
+                    onChangeText={setTemperature}
+                    value={temperature}
+                  />
+                  <Text>Humidity</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Humidity"
+                    keyboardType="numeric"
+                    onChangeText={setHumidity}
+                    value={humidity}
+                  />
+                  <Text>Precipitation</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Precipitation"
+                    keyboardType="numeric"
+                    onChangeText={setPrecipitation}
+                    value={precipitation}
+                  />
+                  <Text>Date</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Date"
+                    keyboardType="numeric"
+                    onChangeText={setDate}
+                    value={date}
+                  />
+                    <Button
+                      title="Upload"
+                      onPress={() => postWeather()} />
+                  </View></>
           )}
+          
         </View>
         </SafeAreaView>
       );
--- weatherapi.go
+++ weatherapi.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	// "github.com/gin-contrib/cors"
 )
 
 //data class for weather obj (structured off database columns)
@@ -18,6 +22,10 @@ type JSONdata struct{
 	Data interface{} `json:"data"`
 }
 
+var weather_vals = []WeatherData{
+		{ID: 1, Temp: 100, Humid: 40, Percip: 20, Date: "02/22/23"},
+		{ID: 2, Temp: 54, Humid: 66, Percip: 0, Date: "02/23/23"},
+	}
 /*
 GET
 general get method send data from server side to client side
@@ -26,22 +34,112 @@ general get method send data from server side to client side
 func getData(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
 	//data to return from the api call
-	weather_vals := []WeatherData{
-		{ID: 1, Temp: 100, Humid: 40, Percip: 20, Date: "02/22/23"},
-		{ID: 2, Temp: 54, Humid: 66, Percip: 0, Date: "02/23/23"},
-	}
+	
 
 	d := JSONdata{Data: weather_vals}
 
 	c.IndentedJSON(http.StatusOK, d)
 }
 
+func insertData(c *gin.Context){
+	c.Header("Access-Control-Allow-Origin", "*")
+	
+	var newWeather WeatherData
+	
+    if err := c.BindJSON(&newWeather); err != nil {
+		return
+    }
+	
+    weather_vals = append(weather_vals, newWeather)
+	
+	d := JSONdata{Data: weather_vals}
+
+	c.IndentedJSON(http.StatusCreated, d)
+}
+
+func getDataByID(id int, c *gin.Context){
+	c.Header("Access-Control-Allow-Origin", "*")
+
+	data, err := getdataid(id)
+	if err != nil{
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"Data not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, data)
+}
+
+
+func getdataid(id int) (JSONdata, error){
+	for _, i := range(weather_vals){
+		if i.ID == id{
+			return JSONdata{Data: i}, nil
+		}
+	}
+	return JSONdata{}, fmt.Errorf("Not Found")
+}
+
+
+func updateData(id int, c *gin.Context){
+	c.Header("Access-Control-Allow-Origin", "*")
+	var data WeatherData
+	if err := c.BindJSON(&data); err != nil{
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error":"invalid request"})
+		return
+	}
+	for i, d := range weather_vals{
+		if d.ID == id{
+			weather_vals[i] = data
+			weatherData := JSONdata{Data: weather_vals}
+			c.IndentedJSON(http.StatusOK, weatherData)
+			return
+		}
+	}
+
+	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "data not found"})
+
+}
+
+
 //router driver make all get and post requests here
 func main() {
 	router := gin.Default()
 
 	//*GET METHOD- data from database
-	router.GET("/data", getData)
+	router.GET("/data", func(c *gin.Context) {
+		id := c.Query("id")
+		switch{
+			case len(id) > 0:
+				idINT, err := strconv.Atoi(id)
+				if err != nil{
+					return
+				}
+				getDataByID(idINT, c)
+			default:
+				getData(c) 
+		}
+	})
+
+	//*POST METHOD - data insert database
+	router.POST("/data", insertData)
+
+	//*PATCH METHOD - data update database
+	router.PATCH("/data", func(c *gin.Context){
+		id := c.Query("id")
+		switch{
+		case len(id) > 0:
+			idINT, err := strconv.Atoi(id)
+			if err != nil{
+				return
+			}
+			updateData(idINT, c)
+		}
+	})
+
+	// config := cors.DefaultConfig()
+	// config.AllowOrigins = []string{"*"}
+
+	// router.Use(cors.New(config))
 
 	//*activate the server running on port 8080
 	router.Run("localhost:8080")
